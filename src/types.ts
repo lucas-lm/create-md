fd83e847e7e83ba802495e037f2aa99239d1240c
Lucas Miranda <lucas-m@outlook.com>
2020-08-09 21:57:21 -0300

    feat: add project types

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,22 @@
 // export types
+
+interface Author {
+  name: string
+  email?: string
+  url?: string
+}
+
+interface Repo {
+  type: string
+  url: string
+}
+
+interface ProjectData {
+  name?: string
+  version?: string
+  author?: string | Author
+  description?: string
+  license?: string
+  [key: string]: any
+}
+
